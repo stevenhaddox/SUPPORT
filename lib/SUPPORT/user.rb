25efe1fd7fd30ebd37bdb0e8b999339e37b3e255
Steven Haddox <steven@haddox.us>
2013-03-29 23:06:27 -0400

    Add Ability to Enable/Disable a User in Config
    * Add attribute in config & User model.
    * Enabled users can be found by default
    * Disabled users can be found via option to ServerUser.find
    * Enabled & Disabled users still returned by ServerUser.all

--- lib/SUPPORT/user.rb
+++ lib/SUPPORT/user.rb
@@ -1,11 +1,12 @@
 module SUPPORT
   class User
-    attr_accessor :role, :username, :password
+    attr_accessor :role, :username, :password, :enabled
 
     def initialize(data)
       @role = data[:role]
       @username = data[:username]
       @password = data[:password]
+      @enabled  = data[:enabled]
     end
 
   end
