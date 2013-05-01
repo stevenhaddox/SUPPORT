a2eef5db6f15ece53a198304e10bf77940ddabea
Steven Haddox <steven@haddox.us>
2013-04-30 22:05:00 -0400

    ServerUser.enabled method to return enabled users

--- spec/lib/SUPPORT/server_user_spec.rb
+++ spec/lib/SUPPORT/server_user_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 module SUPPORT
   describe ServerUser do
     let(:server_user) { FactoryGirl.build(:server_user, :primary) }
-  
+
     describe ".initialize" do
       context "valid params" do
-        let(:params) do 
+        let(:params) do
           {:role => "primary"}
-        end 
+        end
 
         it "should instantiate with valid params" do
           described_class.new(params).class.should == SUPPORT::ServerUser
         end
       end
- 
-      context "invalid params" do 
+
+      context "invalid params" do
         let(:params) do
           Hash.new
         end
@@ -24,19 +24,26 @@ module SUPPORT
           lambda { described_class.new(params) }.should raise_error(KeyError)
         end
       end
-  
+
       it "should assign users" do
         server_user.users.count.should > 0
       end
     end
-  
+
     describe ".all" do
       it "should return a collection of all users" do
         server_user.all.count.should == 4
         server_user.all.map(&:class).uniq.should == [SUPPORT::User]
       end
     end
-  
+
+    describe ".enabled" do
+      it "should return a collection of all users that are enabled" do
+        enabled_users = server_user.all.map{|u| u if u.enabled? }
+        server_user.enabled.should == enabled_users
+      end
+    end
+
     describe ".find" do
       it "should return the user matching the role given" do
         user = server_user.find('app')
@@ -45,7 +52,7 @@ module SUPPORT
         user.password.should == "vagrant"
         user.should be_enabled
       end
-  
+
       it "should return the user found when given a role as a symbol" do
         user = server_user.find(:install)
         user.role.should     == "install"
@@ -53,12 +60,12 @@ module SUPPORT
         user.password.should == "vagrant"
         user.should be_enabled
       end
-  
+
       it "should not return a user that is not enabled" do
         user = server_user.find(:root)
         user.should be_nil
       end
-  
+
       it "should return a disabled user when sent the param ignore_disabled" do
         user = server_user.find(:root, {:include_disabled => true})
         user.role.should     == "root"
@@ -67,7 +74,7 @@ module SUPPORT
         user.should_not be_enabled
       end
     end
-  
+
     describe ".find_by_role_priority" do
       it "should return the first enabled user matching the order of roles given" do
         prioritized_roles = %w(root personal install app)
@@ -77,6 +84,6 @@ module SUPPORT
         user.should be_enabled
       end
     end
-  
+
   end
 end
